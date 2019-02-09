1a625dd9fe1b002a3c296f1e28789fc01980dcfa
wbpandos <whiteboypandos@me.com>
2019-02-08 18:52:55 -0800

    random javascript clock

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+function clock() {
+    let fullDate = new Date();
+    let hours = fullDate.getHours();
+    let mins = fullDate.getMinutes();
+    let sec = fullDate.getSeconds();
+
+    if (hours > 12) {
+        hours = "0" + hours - 12;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    
+    document.getElementById('hour').innerHTML = hours;
+    document.getElementById('minute').innerHTML = ":" + mins;
+    document.getElementById('second').innerHTML = ":" + sec;
+}
+setInterval(clock, 1000);
